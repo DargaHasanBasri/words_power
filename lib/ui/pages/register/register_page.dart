8ace996dde3b0af1110dd72a9828878a40f4a9ef
dargahasanbasri <hasan.basri.darga@naylalabs.com>
2024-02-11 05:40:14 +0300

    Add login-register template

---
+++ lib/ui/pages/register/register_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Register Page"),
+        ],
+      ),
+    );
+  }
+}
