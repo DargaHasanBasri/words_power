bc05eda0d42d8f0dc1453f1e724575616411fa76
DargaHasanBasri <85760132+DargaHasanBasri@users.noreply.github.com>
2024-06-21 14:41:54 +0300

    Test build

--- lib/ui/pages/welcome/welcome_page.dart
+++ lib/ui/pages/welcome/welcome_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:words_power/export.dart';
 
 class WelcomePage extends StatefulWidget {
@@ -12,58 +13,64 @@ class _WelcomePageState extends BaseStatefulState<WelcomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: CustomColors.backgroundColor.withOpacity(0.6),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          AspectRatio(
-            aspectRatio: 0.8,
-            child: FadedWidget(
-              child: Image.asset(
-                'images/welcome2.png',
-                fit: BoxFit.fitWidth,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            AspectRatio(
+              aspectRatio: 0.8,
+              child: FadedWidget(
+                child: Image.asset(
+                  'images/welcome2.png',
+                  fit: BoxFit.fitWidth,
+                ),
               ),
             ),
-          ),
-          CircleAvatar(
-            backgroundColor: CustomColors.buttonBackground,
-            radius: 32,
-            child: Image.asset('images/logo.png'),
-          ),
-          const SizedBox(height: 20),
-          Center(
-            child: Text(
-              'Words-Power',
-              style: TextStyle(
-                color: CustomColors.whiteSmoke,
-                fontSize: 22,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ),
-          const Spacer(),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: CustomButton(
-              onClick: () {
-                appRoutes.navigateTo(Routes.login);
-              },
-              title: 'Login',
+            CircleAvatar(
               backgroundColor: CustomColors.buttonBackground,
+              radius: 32,
+              child: Image.asset('images/logo.png'),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
-            child: CustomButton(
-              onClick: () {
-                appRoutes.navigateTo(Routes.register);
-              },
-              title: 'Register',
-              titleColor: CustomColors.blueCloudBurst,
-              backgroundColor: CustomColors.whiteSmoke,
+            const SizedBox(height: 20),
+            Center(
+              child: Text(
+                'Words-Power',
+                style: TextStyle(
+                  color: CustomColors.whiteSmoke,
+                  fontSize: 22,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
             ),
-          ),
-          const SizedBox(height: 20),
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 24,
+                vertical: 50,
+              ),
+              child: Column(
+                children: [
+                  CustomButton(
+                    onClick: () {
+                      appRoutes.navigateTo(Routes.login);
+                    },
+                    title: 'Login',
+                    backgroundColor: CustomColors.buttonBackground,
+                  ),
+                  const SizedBox(height: 14),
+                  CustomButton(
+                    onClick: () {
+                      appRoutes.navigateTo(Routes.register);
+                    },
+                    title: 'Register',
+                    titleColor: CustomColors.blueCloudBurst,
+                    backgroundColor: CustomColors.whiteSmoke,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+          ],
+        ),
       ),
     );
   }
