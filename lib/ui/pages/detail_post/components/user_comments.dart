013853307178ed92a86c6cb0163aa7e72c3aaf08
DargaHasanBasri <85760132+DargaHasanBasri@users.noreply.github.com>
2024-08-06 21:45:52 +0300

    Complete UI of detail page

---
+++ lib/ui/pages/detail_post/components/user_comments.dart
@@ -0,0 +1,17 @@
+import '../../../../export.dart';
+
+class UserComments extends StatelessWidget {
+  const UserComments({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('İlk Yorum...'),
+      ],
+    );
+  }
+}
